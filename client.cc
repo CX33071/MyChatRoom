94373e665cd92a9f0ef02c199e918230288c9f70
CX33071 <3541053286@qq.com>
2026-06-22 12:51:12 +0800

    note

--- client.cc
+++ client.cc
@@ -12,11 +12,43 @@ std::mutex c_mutex;
 std::mutex msg_mutex;
 bool connok = false;
 bool is_login = false;
-bool recive = false;
+std::atomic<bool> recive{false};
 bool friendrecive = false;
+bool is_addchoice = false;
+bool input=false;
+bool input_ok = false;
+enum Inputstatus {
+    menu,
+    event_confirm,
+    none,
+    chat,
+    cinaccount,
+    cinkey,
+    cincode,
+    cinnum,
+    cinyn,
+    cinfriend,
+    cingroupname
+};
+
+Inputstatus status=Inputstatus::menu;
+struct Inputbag{
+    std::string content;
+    Inputstatus s;
+};
+std::queue<Inputbag> input_queue;
+std::mutex input_mutex;
+std::condition_variable input_cv;
 std::unordered_map<std::string, std::queue<json>> msg_map;
 std::condition_variable msg_cv;
+int choice;
+std::string num;
 std::string account;
+std::string friendaccount;
+std::string code;
+std::string key;
+std::string ss;
+std::string groupname;
 std::string friendtarget;
 struct Event {
     enum Type { FriendRequest, GroupInvite } type;
@@ -24,9 +56,9 @@ struct Event {
 };
 std::queue<Event> event_queue;
 std::mutex event_mutex;
-bool running = true;
+std::atomic<bool> running{true};
 std::condition_variable event_cv;
-std::string cinkey() {
+std::string inputkey() {
     struct termios oldt, newt;
     tcgetattr(STDIN_FILENO, &oldt);
     newt = oldt;
@@ -37,6 +69,36 @@ std::string cinkey() {
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
     return key;
+}
+bool startinput = false;
+void input_thread() {
+    while (1) {
+        if(startinput){
+            std::string line;
+            if (status == Inputstatus::cinkey) {
+                line = inputkey();
+            } else {
+                std::getline(std::cin, line);
+            }
+            {
+                std::lock_guard<std::mutex> lock(input_mutex);
+                while (!input_queue.empty())
+                    input_queue.pop();
+                input_queue.push({line, status});
+                input_cv.notify_one();
+            }
+            startinput = false;
+        }
+    }
+}
+std::string waitinputbase(Inputstatus currentstatus) {
+    status = currentstatus;
+    std::unique_lock<std::mutex> lock(input_mutex);
+    startinput = true;
+    input_cv.wait(lock, [] { return !input_queue.empty() || !running; });
+    auto input = input_queue.front();
+    input_queue.pop();
+    return input.content;
 }
 void connectioncallback(const TcpClient::TcpConnectionPtr& conn) {
     if (conn->connected()) {
@@ -61,7 +123,7 @@ void messagecallback(const TcpClient::TcpConnectionPtr& conn,
 
         json j;
         j = json::parse(msg);
-        std::string cmd = j["data"];
+        std::string cmd = j["cmd"];
         {
             std::lock_guard<std::mutex> lock(msg_mutex);
             msg_map[cmd].push(j);
@@ -69,16 +131,14 @@ void messagecallback(const TcpClient::TcpConnectionPtr& conn,
         }
     }
 }
+
 void handle_message() {
     while (running) {
-        if(!running){
-            break;
-        }
         std::unique_lock<std::mutex> lock(msg_mutex);
         msg_cv.wait(lock, [] { return !msg_map.empty(); });
         for (auto it = msg_map.begin(); it != msg_map.end();) {
             std::string cmd = it->first;
-            std::queue<json> &q = it->second;
+            std::queue<json>& q = it->second;
             while (!q.empty()) {
                 json msg = q.front();
                 q.pop();
@@ -93,7 +153,6 @@ void handle_message() {
                     {
                         std::lock_guard<std::mutex> lock(event_mutex);
                         event_queue.push(e);
-                        event_cv.notify_one();
                     }
                 } else if (cmd == "invitedres") {
                     Event e;
@@ -106,13 +165,11 @@ void handle_message() {
                     }
                 } else if (cmd == "keysignin_res") {
                     std::string code = msg["code"];
-                    std::cout << code << std::endl;
-
                     if (code == "1") {
                         is_login = true;
                     }
                     std::string data = msg["data"];
-                    std::cout << "\n[系统消息]: " << data << std::endl;
+                    std::cout << "\n[系统消息]: " << data <<std::endl;
                 } else if (cmd == "codesignin_res") {
                     std::string code = msg["code"];
                     if (code == "1") {
@@ -122,6 +179,7 @@ void handle_message() {
                     std::cout << "\n[系统消息]: " << data << std::endl;
                 } else {
                     std::string data = msg["data"];
+                   
                     std::cout << "\n[系统消息]: " << data << std::endl;
                 }
                 recive = true;
@@ -134,51 +192,53 @@ void handle_message() {
         }
     }
 }
-void handle_event() {
-    while (1) {
-        if(!running){
-            break;
+
+
+bool if_event(Event& e){
+    std::lock_guard<std::mutex> lock(event_mutex);
+    if(event_queue.empty()){
+        return false;
+    }
+    e = event_queue.front();
+    event_queue.pop();
+    return true;
+}
+void handle_event(Event e){
+    json reply;
+    if (e.type == Event::FriendRequest) {
+        std::cout << "\n好友申请:" << e.data["message"] << '\n'
+                  << "请输入是否同意y/n: " << std::endl;
+        std::string choice = waitinputbase(Inputstatus::event_confirm);
+        if (choice == "y" || choice == "Y") {
+            reply["cmd"] = "agreefriend";
+            std::cout << "已同意好友申请" << std::endl;
+        } else {
+            reply["cmd"] = "refusefriend";
+            std::cout << "已经拒绝好友申请" << std::endl;
         }
-        Event e;
-        {
-            std::unique_lock<std::mutex> lock(event_mutex);
-            event_cv.wait(lock, [] { return !event_queue.empty(); });
-            e = event_queue.front();
-            event_queue.pop();
+        reply["account"] = account;
+        reply["friendaccount"] = e.data["target"];
+        c_conn->send(reply.dump() + '\n');
+        status = Inputstatus::menu;
+
+    } else if (e.type == Event::GroupInvite) {
+        std::cout << "\n群邀请:" << e.data["data"] << std::endl;
+        std::cout << "请输入是否同意y/n: ";
+        std::string choice = waitinputbase(Inputstatus::event_confirm);
+        if (choice == "y" || choice == "Y") {
+            reply["cmd"] = "agreejoin";
+            std::cout << "已同意入群申请" << std::endl;
+        } else {
+            reply["cmd"] = "refusegroup";
+            std::cout << "已经拒绝入群申请" << std::endl;
         }
-        if (e.type == Event::FriendRequest) {
-            std::cout << "\n好友申请:" << e.data["message"] << '\n'<<"请输入是否同意y/n: "<<std::endl;
-            std::string choice;
-            std::getline(std::cin >> std::ws, choice);
-            json reply;
-            if (choice == "y") {
-                reply["cmd"] = "agreefriend";
-            } else {
-                reply["cmd"] = "refusefriend";
-            }
-            reply["account"] = account;
-            reply["friendaccount"] = e.data["target"];
-            c_conn->send(reply.dump() + '\n');
-            friendrecive = true;
-        } else if (e.type == Event::GroupInvite) {
-            std::cout << "\n群邀请:" << e.data["data"] << std::endl;
-            std::cout << "请输入是否同意y/n: ";
-            std::string choice;
-            std::getline(std::cin >> std::ws, choice);
-            json reply;
-            if(choice=="y") {
-                reply["cmd"] = "agreejoin";
-            } else {
-                reply["cmd"] = "refusegroup";
-            }
-            reply["account"] = account;
-            reply["groupname"] = e.data["groupname"];
-            reply["target"] = e.data["account"];
-            c_conn->send(reply.dump() + '\n');
-        }
+        reply["account"] = account;
+        reply["groupname"] = e.data["groupname"];
+        reply["target"] = e.data["account"];
+        c_conn->send(reply.dump() + '\n');
+        status = Inputstatus::menu;
     }
 }
-
 void main_menu() {
     std::cout << "\n";
     std::cout << "欢迎使用MyChatRoom!" << std::endl;
@@ -189,7 +249,8 @@ void main_menu() {
     std::cout << "4.忘记密码\n";
     std::cout << "5.注销账号\n";
     std::cout << "0.退出\n";
-    std::cout << "请选择:";
+    std::cout << "请选择:" << std::endl;
+    ;
 }
 void friend_menu() {
     std::cout << "\n好友管理\n";
@@ -203,30 +264,37 @@ void friend_menu() {
     std::cout << "13.删除群聊\n";
     std::cout << "14.群聊天\n";
     std::cout << "15. 返回主菜单\n";
-    std::cout << "请选择:";
+    std::cout << "请选择:" << std::endl;
+    ;
 }
 
 void mainfunction() {
+    std::string password;
+    std::string verifycode;
+    json j;
+    std::string frienduser;
+    std::string servermsg;
+    std::string chatmsg;
+    std::string groupname;
+    std::string groupmsg;
+    std::string ss;
     json res;
     while (1) {
+        Event e;
+        while(if_event(e)){
+            handle_event(e);
+        }
         if (!is_login) {
             main_menu();
         } else {
             friend_menu();
         }
-
-        int choice;
-        std::cin >> choice;
-        std::string password;
-        std::string verifycode;
-        json j;
-        std::string frienduser;
-        std::string servermsg;
-        std::string chatmsg;
-        std::string groupname;
-        std::string groupmsg;
-        std::string ss;
-        json res;
+        while (if_event(e)) {
+            handle_event(e);
+        }
+        status = Inputstatus::menu;
+        std::string input = waitinputbase(Inputstatus::menu);
+        int choice = std::stoi(input);
         switch (choice) {
             case 1:
                 if (is_login) {
@@ -234,18 +302,20 @@ void mainfunction() {
                     break;
                 }
                 std::cout << "请输入你的qq邮箱:";
-                std::getline(std::cin >> std::ws, account);
+                account = waitinputbase(Inputstatus::cinaccount);
                 std::cout << "请输入你的密码:";
-                password = cinkey();
+                status = Inputstatus::cinkey;
+                password = waitinputbase(Inputstatus::cinkey);
                 std::cout << std::endl;
                 j["cmd"] = "signup";
                 j["account"] = account;
                 j["password"] = password;
                 c_conn->send(j.dump() + '\n');
                 std::cout << "注册请求已经发送!" << std::endl;
-                while(!recive){
-
+                recive = false;
+                while (!recive) {
                 }
+                recive = false;
                 break;
             case 2:
                 if (is_login) {
@@ -253,29 +323,33 @@ void mainfunction() {
                     break;
                 }
                 std::cout << "请先输入你的qq邮箱:";
-                std::getline(std::cin >> std::ws, account);
+                account = waitinputbase(Inputstatus::cinaccount);
                 j["cmd"] = "verifycode";
                 j["account"] = account;
                 c_conn->send(j.dump() + '\n');
                 std::cout << "验证码已经发送到您的qq邮箱\n";
                 std::cout << "请输入验证码:";
-                std::cin >> verifycode;
+                verifycode = waitinputbase(Inputstatus::cincode);
                 j["cmd"] = "verifycodesignin";
                 j["account"] = account;
                 j["code"] = verifycode;
                 c_conn->send(j.dump() + '\n');
+                recive = false;
                 while (!recive) {
                 }
+                recive = false;
                 break;
             case 3:
                 if (is_login) {
                     std::cout << "请重新输入6-15之间数字!" << std::endl;
                     break;
                 }
-                std::cout << "请先输入你的qq邮箱:";
-                std::getline(std::cin >> std::ws, account);
+                std::cout << "请先输入你的qq邮箱:"<<std::endl;
+                account = waitinputbase(Inputstatus::cinaccount);
+               
                 std::cout << "请输入您的密码:";
-                password = cinkey();
+                status=Inputstatus::cinkey;
+                password = waitinputbase(Inputstatus::cinkey);
                 std::cout << std::endl;
                 j["cmd"] = "keysignin";
                 j["account"] = account;
@@ -291,7 +365,12 @@ void mainfunction() {
                     break;
                 }
                 std::cout << "请输入你的qq邮箱: ";
-                std::getline(std::cin >> std::ws, account);
+                status = Inputstatus::cinaccount;
+                input = true;
+                while (!input_ok) {
+                }
+                input_ok = false;
+
                 j["cmd"] = "forgetkey";
                 j["account"] = account;
                 c_conn->send(j.dump() + '\n');
@@ -305,7 +384,7 @@ void mainfunction() {
                     break;
                 }
                 std::cout << "请输入您的密码:";
-                std::getline(std::cin >> std::ws, password);
+                password = inputkey();
                 j["cmd"] = "destory";
                 j["account"] = account;
                 j["password"] = password;
@@ -316,18 +395,19 @@ void mainfunction() {
                     std::cout << "请重新输入6-15之间数字!" << std::endl;
                     break;
                 }
-                std::cout << "再见！" << std::endl;
-                running = false;
-                msg_cv.notify_all();
-                event_cv.notify_all();
-                return;
+                    std::cout << "再见！" << std::endl;
+                    running = false;
+                    msg_cv.notify_all();
+                    event_cv.notify_all();
+                    return;
+                
             case 6:
                 if (!is_login) {
                     std::cout << "请先登录!" << std::endl;
                     break;
                 }
                 std::cout << "请输入要添加好友的账号:";
-                std::getline(std::cin >> std::ws, frienduser);
+                frienduser = waitinputbase(Inputstatus::cinfriend);
                 j["cmd"] = "addfriend";
                 j["from"] = account;
                 j["to"] = frienduser;
@@ -350,7 +430,12 @@ void mainfunction() {
                     break;
                 }
                 std::cout << "请输入要拉黑的好友账号:";
-                std::getline(std::cin >> std::ws, frienduser);
+                status = Inputstatus::cinfriend;
+                input = true;
+                while (!input_ok) {
+                }
+                input_ok = false;
+
                 j["cmd"] = "block";
                 j["account"] = account;
                 j["target"] = frienduser;
@@ -362,7 +447,12 @@ void mainfunction() {
                     break;
                 }
                 std::cout << "请输入要删除的好友账号:";
-                std::getline(std::cin >> std::ws, frienduser);
+                status = Inputstatus::cinfriend;
+                input = true;
+                while (!input_ok) {
+                }
+                input_ok = false;
+
                 j["cmd"] = "delfriend";
                 j["account"] = account;
                 j["target"] = frienduser;
@@ -374,7 +464,12 @@ void mainfunction() {
                     break;
                 }
                 std::cout << "请输入要创建的群聊的名字:";
-                std::getline(std::cin >> std::ws, groupname);
+                status = Inputstatus::cingroupname;
+                input = true;
+                while (!input_ok) {
+                }
+                input_ok = false;
+
                 j["cmd"] = "creategroup";
                 j["account"] = account;
                 j["groupname"] = groupname;
@@ -386,9 +481,19 @@ void mainfunction() {
                     break;
                 }
                 std::cout << "请输入要邀请好友加入的群聊名称:";
-                std::getline(std::cin >> std::ws, groupname);
+                status = Inputstatus::cingroupname;
+                input = true;
+                while (!input_ok) {
+                }
+                input_ok = false;
+
                 std::cout << "请输入要邀请哪位好友加入该群聊:";
-                std::getline(std::cin >> std::ws, frienduser);
+                status = Inputstatus::cinfriend;
+                input = true;
+                while (!input_ok) {
+                }
+                input_ok = false;
+
                 j["cmd"] = "invite";
                 j["account"] = account;
                 j["groupname"] = groupname;
@@ -402,7 +507,12 @@ void mainfunction() {
                     break;
                 }
                 std::cout << "请输入要删除的群聊的名称:";
-                std::getline(std::cin >> std::ws, groupname);
+                status = Inputstatus::cingroupname;
+                input = true;
+                while (!input_ok) {
+                }
+                input_ok = false;
+
                 j["cmd"] = "delgroup";
                 j["account"] = account;
                 j["groupname"] = groupname;
@@ -419,11 +529,12 @@ void mainfunction() {
                 break;
 
             default:
-                std::cout << choice;
+
                 std::cout << "请输入有效选项!" << std::endl;
                 exit(0);
                 break;
         };
+        input = false;
     }
 }
 int main(int argc, char* argv[]) {
@@ -435,12 +546,11 @@ int main(int argc, char* argv[]) {
     client.connect();
     std::thread t1(mainfunction);
     std::thread t2(handle_message);
-    std::thread t3(handle_event);
+    std::thread t4(input_thread);
     int timeout = -1;
     loop.loop(timeout);
     msg_cv.notify_all();
     event_cv.notify_all();
     t2.join();
-    t3.join();
     return 0;
 }
